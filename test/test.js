77f12fc30a67e80f1ebe69a6e56fab70f0589c23
pongtsu <pongtsu@bitbucket.com>
2015-07-13 15:14:22 +0700

    try more again

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var C = require('../cash.js');
 
 
 describe('Cash Register', function(){
-  describe('Module C', function(){asd
+  describe('Module C', function(){
     it('should have a getChange Method', function(){
       assert.equal(typeof C, 'object');
       assert.equal(typeof C.getChange, 'function');
