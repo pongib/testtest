cf576d6567df9b8204f8c12d081511301fb90f23
pongtsu <pongtsu@bitbucket.com>
2015-07-13 15:12:08 +0700

    try again naja

--- test/test.js
+++ test/test.js
@@ -3,11 +3,11 @@ var C = require('../cash.js');
 
 
 describe('Cash Register', function(){
-  describe('Module C', function(){
+  describe('Module C', function(){asd
     it('should have a getChange Method', function(){
       assert.equal(typeof C, 'object');
       assert.equal(typeof C.getChange, 'function');
-    });asd
+    });
 
     it('getChange(210, 300) should equal [50, 20, 20]', function(){
       assert.deepEqual(C.getChange(210, 300), [50, 20, 20]);
